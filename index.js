69b9cc3abf4f66714c257b684b92c8e519c7e34e
Alexander Cederblad <alexcederblad@gmail.com>
2017-11-25 12:18:56 +0200

    Fix dotenv configure to config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 
-dotenv.configure()
+dotenv.config()
 app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
